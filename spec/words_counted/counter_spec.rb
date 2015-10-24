69214a8cbb97a4bebc2b33c25d451802edd62400
Mohamad El-Husseini <husseini.mel@gmail.com>
2015-10-24 18:34:29 -0200

    sort_by is unstable and this changes tests to avoid having build fails on different ruby versions

--- spec/words_counted/counter_spec.rb
+++ spec/words_counted/counter_spec.rb
@@ -4,7 +4,7 @@ require_relative "../spec_helper"
 module WordsCounted
   describe Counter do
     let(:counter) do
-      tokens = WordsCounted::Tokeniser.new("We are all in the gutter, but some of us are looking at the stars.").tokenise
+      tokens = WordsCounted::Tokeniser.new("one three three three woot woot").tokenise
       Counter.new(tokens)
     end
 
@@ -16,27 +16,26 @@ module WordsCounted
 
     describe "#token_count" do
       it "returns the correct number of tokens" do
-        expect(counter.token_count).to eq(15)
+        expect(counter.token_count).to eq(6)
       end
     end
 
     describe "#uniq_token_count" do
       it "returns the number of unique token" do
-        expect(counter.uniq_token_count).to eq(13)
+        expect(counter.uniq_token_count).to eq(3)
       end
     end
 
     describe "#char_count" do
       it "returns the correct number of chars" do
-        expect(counter.char_count).to eq(50)
+        expect(counter.char_count).to eq(26)
       end
     end
 
     describe "#token_frequency" do
       it "returns a two-dimensional array where each member array is a token and its frequency in descending order" do
         expected = [
-          ['are', 2], ['the', 2], ['but', 1], ['us', 1], ['of', 1], ['some', 1], ['looking', 1],
-          ['gutter', 1], ['at', 1], ['in', 1], ['all', 1], ['stars', 1], ['we', 1]
+          ['three', 3], ['woot', 2], ['one', 1]
         ]
         expect(counter.token_frequency).to eq(expected)
       end
@@ -45,8 +44,7 @@ module WordsCounted
     describe "#token_lengths" do
       it "returns a two-dimensional array where each member array is a token and its length in descending order" do
         expected = [
-          ['looking', 7], ['gutter', 6], ['stars', 5], ['some', 4], ['but', 3], ['the', 3],
-          ['are', 3], ['all', 3], ['us', 2], ['in', 2], ['at', 2], ['of', 2], ['we', 2]
+          ['three', 5], ['woot', 4], ['one', 3]
         ]
         expect(counter.token_lengths).to eq(expected)
       end
@@ -55,16 +53,14 @@ module WordsCounted
     describe "#token_density" do
       it "returns a two-dimensional array where each member array is a token and its density in descending order" do
         expected = [
-          ['are', 0.13], ['the', 0.13], ['looking', 0.07], ['in', 0.07], ['at', 0.07],['gutter', 0.07],
-          ['all', 0.07], ['some', 0.07], ['of', 0.07], ['us', 0.07], ['but', 0.07], ['stars', 0.07], ['we', 0.07]
+          ['three', 0.5], ['woot', 0.33], ['one', 0.17]
         ]
         expect(counter.token_density).to eq(expected)
       end
 
       it "accepts a precision" do
         expected = [
-          ['are', 0.1333], ['the', 0.1333], ['looking', 0.0667], ['in', 0.0667], ['at', 0.0667], ['gutter', 0.0667],
-          ['all', 0.0667], ['some', 0.0667], ['of', 0.0667], ['us', 0.0667], ['but', 0.0667], ['stars', 0.0667], ['we', 0.0667]
+          ['three', 0.5], ['woot', 0.3333], ['one', 0.1667]
         ]
         expect(counter.token_density(precision: 4)).to eq(expected)
       end
@@ -73,8 +69,7 @@ module WordsCounted
     describe "#most_frequent_tokens" do
       it "returns a hash of the tokens with the highest frequency, where each key a token, and each value is its frequency" do
         expected = {
-          'are' => 2,
-          'the' => 2
+          'three' => 3
         }
         expect(counter.most_frequent_tokens).to eq(expected)
       end
@@ -83,7 +78,7 @@ module WordsCounted
     describe "#longest_tokens" do
       it "returns a hash of the tokens with the highest length, where each key a token, and each value is its length" do
         expected = {
-          'looking' => 7
+          'three' => 5
         }
         expect(counter.longest_tokens).to eq(expected)
       end
--- spec/words_counted/deprecated_spec.rb
+++ spec/words_counted/deprecated_spec.rb
@@ -6,23 +6,21 @@ module WordsCounted
 
   describe Counter do
     let(:counter) do
-      tokens = WordsCounted::Tokeniser.new("We are all in the gutter, but some of us are looking at the stars.").tokenise
+      tokens = WordsCounted::Tokeniser.new("one three three three woot woot").tokenise
       Counter.new(tokens)
     end
 
     describe "#word_density" do
       it "returns words and their density in percent" do
         expected = [
-          ['are', 13.33], ['the', 13.33], ['looking', 6.67], ['in', 6.67], ['at', 6.67], ['gutter', 6.67],
-          ['all', 6.67], ['some', 6.67], ['of', 6.67], ['us', 6.67], ['but', 6.67], ['stars', 6.67], ['we', 6.67]
+          ['three', 50.0], ['woot', 33.33], ['one', 16.67]
         ]
         expect(counter.word_density).to eq(expected)
       end
 
       it "accepts a precision" do
         expected = [
-          ['are', 13.3333], ['the', 13.3333], ['looking', 6.6667], ['in', 6.6667], ['at', 6.6667], ['gutter', 6.6667],
-          ['all', 6.6667], ['some', 6.6667], ['of', 6.6667], ['us', 6.6667], ['but', 6.6667], ['stars', 6.6667], ['we', 6.6667]
+          ['three', 50.0], ['woot', 33.3333], ['one', 16.6667]
         ]
         expect(counter.word_density(4)).to eq(expected)
       end
@@ -31,8 +29,7 @@ module WordsCounted
     describe "#word_occurrences" do
       it "returns a two dimensional array sorted by descending word occurrence" do
         expected = {
-          'are' => 2, 'the' => 2, 'but' => 1, 'us' => 1, 'of' => 1, 'some' => 1, 'looking' => 1,
-          'gutter' => 1, 'at' => 1, 'in' => 1, 'all' => 1, 'stars' => 1, 'we' => 1
+          'three' => 3, 'woot' => 2, 'one' => 1
         }
         expect(counter.word_occurrences).to eq(expected)
       end
@@ -41,18 +38,16 @@ module WordsCounted
     describe "#sorted_word_occurrences" do
       it "returns a two dimensional array sorted by descending word occurrence" do
         expected = [
-          ['are', 2], ['the', 2], ['but', 1], ['us', 1], ['of', 1], ['some', 1],
-          ['looking', 1], ['gutter', 1], ['at', 1], ['in', 1], ['all', 1], ['stars', 1], ['we', 1]
+          ['three', 3], ['woot', 2], ['one', 1]
         ]
         expect(counter.sorted_word_occurrences).to eq(expected)
       end
     end
 
     describe "#word_lengths" do
-      it "returns a two dimensional array sorted by descending word length" do
+      it "returns a hash of of words and their length sorted descending by length" do
         expected = {
-          'looking' => 7, 'gutter' => 6, 'stars' => 5, 'some' => 4, 'but' => 3, 'the' => 3,
-          'are' => 3, 'all' => 3, 'us' => 2, 'in' => 2, 'at' => 2, 'of' => 2, 'we' => 2
+          'three' => 5, 'woot' => 4, 'one' => 3
         }
         expect(counter.word_lengths).to eq(expected)
       end
@@ -61,27 +56,25 @@ module WordsCounted
     describe "#sorted_word_lengths" do
       it "returns a two dimensional array sorted by descending word length" do
         expected = [
-          ['looking', 7], ['gutter', 6], ['stars', 5], ['some', 4], ['but', 3],
-          ['the', 3], ['are', 3], ['all', 3], ['us', 2], ['in', 2], ['at', 2], ['of', 2], ['we', 2]
+          ['three', 5], ['woot', 4], ['one', 3]
         ]
         expect(counter.sorted_word_lengths).to eq(expected)
       end
     end
 
     describe "#longest_words" do
-      it "returns the longest words" do
+      it "returns a two-dimentional array of the longest words and their lengths" do
         expected = [
-          ['looking', 7]
+          ['three', 5]
         ]
         expect(counter.longest_words).to eq(expected)
       end
     end
 
     describe "#most_occurring_words" do
-      it "returns the longest words" do
+      it "returns a two-dimentional array of words with the highest frequency and their frequencies" do
         expected = [
-          ['are', 2],
-          ['the', 2]
+          ['three', 3]
         ]
         expect(counter.most_occurring_words).to eq(expected)
       end
@@ -89,17 +82,17 @@ module WordsCounted
 
     describe "#average_chars_per_word" do
       it "returns the average number of chars per word" do
-        expect(counter.average_chars_per_word).to eq(3.33)
+        expect(counter.average_chars_per_word).to eq(4.33)
       end
 
       it "accepts precision" do
-        expect(counter.average_chars_per_word(4)).to eq(3.3333)
+        expect(counter.average_chars_per_word(4)).to eq(4.3333)
       end
     end
 
     describe "#count" do
       it "returns count for a single word" do
-        expect(counter.count('are')).to eq(2)
+        expect(counter.count('one')).to eq(1)
       end
     end
   end
