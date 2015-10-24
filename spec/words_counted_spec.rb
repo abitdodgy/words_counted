b26d1fa37f8f11acf17b148af6ebb2574af90187
Mohamad El-Husseini <husseini.mel@gmail.com>
2015-10-24 19:02:00 -0200

    make hound happy

--- spec/words_counted_spec.rb
+++ spec/words_counted_spec.rb
@@ -4,12 +4,12 @@ require_relative "spec_helper"
 describe WordsCounted do
   describe ".from_file" do
     it "opens and reads a text file" do
-      counter = WordsCounted.from_file('spec/support/the_hart_and_the_hunter.txt')
+      counter = WordsCounted.from_file("spec/support/the_hart_and_the_hunter.txt")
       expect(counter.token_count).to eq(139)
     end
 
     it "opens and reads a text file with options" do
-      counter = WordsCounted.from_file('spec/support/the_hart_and_the_hunter.txt', exclude: 'hunter')
+      counter = WordsCounted.from_file("spec/support/the_hart_and_the_hunter.txt", exclude: "hunter")
       expect(counter.token_count).to eq(135)
     end
   end
@@ -21,7 +21,7 @@ describe WordsCounted do
     end
 
     it "returns a counter instance with given input and options" do
-      counter = WordsCounted.count("We are all in the gutter!", exclude: 'the gutter')
+      counter = WordsCounted.count("We are all in the gutter!", exclude: "the gutter")
       expect(counter.tokens).to eq(%w[we are all in])
     end
   end
