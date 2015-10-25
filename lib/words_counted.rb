73404c6c8b1d2e2a64788b08c14ae9aa56b4949c
Mohamad El-Husseini <husseini.mel@gmail.com>
2015-10-24 22:06:51 -0200

    adds missing documentation

--- lib/words_counted.rb
+++ lib/words_counted.rb
@@ -1,6 +1,8 @@
 # -*- encoding : utf-8 -*-
 require "refinements/hash_refinements"
 
+require "words_counted/deprecated"
+
 require "words_counted/tokeniser"
 require "words_counted/counter"
 require "words_counted/version"
@@ -11,11 +13,27 @@ rescue LoadError
 end
 
 module WordsCounted
-  def self.count(string, options = {})
-    tokens = Tokeniser.new(string).tokenise(options)
+  # Takes a string, tokenises it, and returns an instance of Counter
+  # with the resulting tokens.
+  #
+  # @see Tokeniser.tokenise
+  # @see Counter.initialize
+  #
+  # @param [String] input   The input to be tokenised.
+  # @param [Hash] options   The options to pass onto `Counter`.
+  def self.count(input, options = {})
+    tokens = Tokeniser.new(input).tokenise(options)
     Counter.new(tokens)
   end
 
+  # Takes a file path, reads the file and tokenises its contents,
+  # and returns an instance of Counter with the resulting tokens.
+  #
+  # @see Tokeniser.tokenise
+  # @see Counter.initialize
+  #
+  # @param [String] path    The file to be read and tokenised.
+  # @param [Hash] options   The options to pass onto `Counter`.
   def self.from_file(path, options = {})
     tokens = File.open(path) do |file|
       Tokeniser.new(file.read).tokenise(options)
